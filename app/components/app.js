605112657b467306132d77355c4a7cdbba674834
Mehul Patel <mehul8@gmail.com>
2016-07-06 15:49:08 -0700

    move app code to app/components dir

---
+++ app/components/app.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+}
